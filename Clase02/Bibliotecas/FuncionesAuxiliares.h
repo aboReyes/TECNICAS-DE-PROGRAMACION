2254337011a9ad3b5b3f2dee85d74dffc318d58c
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-08-26 10:35:03 -0500

    Create FuncionesAuxiliares.h

---
+++ Clase02/Bibliotecas/FuncionesAuxiliares.h
@@ -0,0 +1,12 @@
+//
+// Created by DIEGO on 22/08/2025.
+//
+
+#ifndef CLASE02_FUNCIONESAUXILIARES_H
+#define CLASE02_FUNCIONESAUXILIARES_H
+
+
+
+long long factorial(int numero);
+
+#endif //CLASE02_FUNCIONESAUXILIARES_H
