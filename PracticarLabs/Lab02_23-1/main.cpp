89363f62ed7d1211cd11c7219a9fb56ba27c44ae
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-08-27 00:21:58 -0500

    Create main.cpp

---
+++ PracticarLabs/Lab02_23-1/main.cpp
@@ -0,0 +1,14 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo: LAB CLASE 1
+*Fecha y Hora:
+*/
+
+#include "Librerias/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+
+    crearReporte();
+
+    return 0;
+}
