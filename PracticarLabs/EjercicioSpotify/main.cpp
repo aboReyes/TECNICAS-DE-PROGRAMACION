01e77b00148331aac1af713b0f74605f9f01f256
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-09-07 14:35:32 -0500

    Create main.cpp

---
+++ PracticarLabs/EjercicioSpotify/main.cpp
@@ -0,0 +1,12 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo:
+*Fecha y Hora: 
+*/
+
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+    crearReporte();
+    return 0;
+}
