92e5dcf7b9e5a50f16ed87e2b5a831b0f9541d10
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-09-14 22:35:04 -0500

    Create main.cpp

---
+++ PracticarLabs/LAB02_24-1/main.cpp
@@ -0,0 +1,12 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo:
+*Fecha y Hora: 16:41 --> 19:00
+*/
+
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+    procesarReporte("ArchivoDatos/CitasMedicas.txt","ArchivoReportes/Reporte.txt");
+    return 0;
+}
