d3e02f598386d3d12a87c11b09754a52b47dc05b
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-09-22 16:37:04 -0500

    Create main.cpp

---
+++ PracticarLabs/EjercicioKPOP/main.cpp
@@ -0,0 +1,16 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo:
+*Fecha y Hora:
+*/
+
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+
+    crearReporte("ArchivoDeDatos/Artistas.txt",
+                    "ArchivoDeDatos/Canciones.txt",
+                    "ArchivoDeDatos/Reproducciones.txt",
+                    "ArchivoDeReportes/ReporteKpop.txt");
+    return 0;
+}
